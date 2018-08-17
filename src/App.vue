eae55305e056b52c96a3a2a8cf8e77d7a793fdef
yuezhenyu <zhenyuyue@qq.com>
2018-08-17 17:34:21 +0800

    初次导入

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <x-header :right-options="{showMore: true}" @on-click-more="showMenus = true">with more menu</x-header>
+    <router-view></router-view>
+    <tabbar>
+      <tabbar-item>
+        <img slot="icon" src="./assets/demo/icon_nav_button.png">
+        <span slot="label">Wechat</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" src="./assets/demo/icon_nav_msg.png">
+        <span slot="label">Message</span>
+      </tabbar-item>
+      <tabbar-item selected link="/component/demo">
+        <img slot="icon" src="./assets/demo/icon_nav_article.png">
+        <span slot="label">Explore</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="./assets/demo/icon_nav_cell.png">
+        <span slot="label">News</span>
+      </tabbar-item>
+    </tabbar>
+    <div v-transfer-dom>
+      <actionsheet :menus="menus" v-model="showMenus" show-cancel></actionsheet>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import { XHeader, Actionsheet, TransferDom, Tabbar, TabbarItem, Group, Cell } from 'vux'
+  export default {
+    directives: {
+      TransferDom
+    },
+    name: 'app',
+    components: {
+      XHeader,
+      Actionsheet,
+      Tabbar,
+      TabbarItem,
+      Group,
+      Cell
+    },
+    data () {
+      return {
+        menus: {
+          menu1: 'Take Photo',
+          menu2: 'Choose from photos'
+        },
+        showMenus: false
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+@import '~vux/src/styles/reset.less';
+
+body {
+  background-color: #fbf9fe;
+}
+</style>
---
+++ src/components/HelloFromVux.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <group>
+      <cell is-link title="Simple" link="/component/tabbar-simple"></cell>
+      <cell is-link title="Switch icons" link="/component/tabbar-icon"></cell>
+    </group>
+    <tabbar>
+      <tabbar-item>
+        <img slot="icon" src="../assets/demo/icon_nav_button.png">
+        <span slot="label">Wechat</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" src="../assets/demo/icon_nav_msg.png">
+        <span slot="label">Message</span>
+      </tabbar-item>
+      <tabbar-item selected link="/component/demo">
+        <img slot="icon" src="../assets/demo/icon_nav_article.png">
+        <span slot="label">Explore</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="../assets/demo/icon_nav_cell.png">
+        <span slot="label">News</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem, Group, Cell } from 'vux'
+
+export default {
+  components: {
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
