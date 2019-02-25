432ec2da8917e66e845fab529e9077b59692f3e7
yuezhenyu <zhenyuyue@qq.com>
2019-02-25 17:13:19 +0800

    调整域名

--- shell/deployweb.sh
+++ shell/deployweb.sh
@@ -1,4 +1,4 @@
 npm run build
-ssh root@202.182.116.51 "rm -rf /opt/javaweb/vue/myweb/*"
-scp -r ../dist/* root@202.182.116.51:/opt/javaweb/vue/myweb/
+ssh root@52tt.xyz "rm -rf /opt/javaweb/vue/myweb/*"
+scp -r ../dist/* root@52tt.xyz:/opt/javaweb/vue/myweb/
 
